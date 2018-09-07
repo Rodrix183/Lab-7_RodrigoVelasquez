32993e3c773b741b655ffc18d071a0e2c0dfc920
rodrix183 <rodrix183@hotmail.com>
2018-09-07 13:42:34 -0600

    Clase Registro

--- Lab7_RodrigoVelasquez/src/lab7_rodrigovelasquez/Tander.java
+++ Lab7_RodrigoVelasquez/src/lab7_rodrigovelasquez/Tander.java
@@ -9,6 +9,25 @@ public class Tander extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jd_Registro = new javax.swing.JDialog();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        tf_registroContra = new javax.swing.JTextField();
+        tf_registroUser = new javax.swing.JTextField();
+        tf_registroNombre = new javax.swing.JTextField();
+        jLabel9 = new javax.swing.JLabel();
+        tf_registroEdad = new javax.swing.JTextField();
+        jLabel10 = new javax.swing.JLabel();
+        rb_M = new javax.swing.JRadioButton();
+        rb_F = new javax.swing.JRadioButton();
+        jLabel11 = new javax.swing.JLabel();
+        rb_M1 = new javax.swing.JRadioButton();
+        rb_F1 = new javax.swing.JRadioButton();
+        jButton2 = new javax.swing.JButton();
+        buttonGroup1 = new javax.swing.ButtonGroup();
+        buttonGroup2 = new javax.swing.ButtonGroup();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
@@ -16,6 +35,124 @@ public class Tander extends javax.swing.JFrame {
         tf_user = new javax.swing.JTextField();
         pf_contra = new javax.swing.JPasswordField();
         jLabel4 = new javax.swing.JLabel();
+
+        jLabel5.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel5.setText("Registro");
+
+        jLabel6.setText("Usuario");
+
+        jLabel7.setText("Nombre Completo");
+
+        jLabel8.setText("Contraseña");
+
+        jLabel9.setText("Edad");
+
+        jLabel10.setText("Sexo");
+
+        buttonGroup1.add(rb_M);
+        rb_M.setText("M");
+
+        buttonGroup1.add(rb_F);
+        rb_F.setText("F");
+
+        jLabel11.setText("Premium");
+
+        buttonGroup2.add(rb_M1);
+        rb_M1.setText("Si");
+
+        buttonGroup2.add(rb_F1);
+        rb_F1.setText("No");
+
+        jButton2.setText("Guardar");
+        jButton2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton2MouseClicked(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jd_RegistroLayout = new javax.swing.GroupLayout(jd_Registro.getContentPane());
+        jd_Registro.getContentPane().setLayout(jd_RegistroLayout);
+        jd_RegistroLayout.setHorizontalGroup(
+            jd_RegistroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jd_RegistroLayout.createSequentialGroup()
+                .addGroup(jd_RegistroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jd_RegistroLayout.createSequentialGroup()
+                        .addGap(160, 160, 160)
+                        .addComponent(jLabel5))
+                    .addGroup(jd_RegistroLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jLabel7)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(tf_registroNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 243, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jd_RegistroLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(jd_RegistroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel8)
+                            .addComponent(jLabel9, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel6, javax.swing.GroupLayout.Alignment.TRAILING))
+                        .addGap(18, 18, 18)
+                        .addGroup(jd_RegistroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(tf_registroContra)
+                            .addComponent(tf_registroUser, javax.swing.GroupLayout.DEFAULT_SIZE, 200, Short.MAX_VALUE)
+                            .addComponent(tf_registroEdad, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(jd_RegistroLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(jd_RegistroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jd_RegistroLayout.createSequentialGroup()
+                                .addComponent(rb_M)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(rb_F))
+                            .addComponent(jLabel10)))
+                    .addGroup(jd_RegistroLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(jd_RegistroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jd_RegistroLayout.createSequentialGroup()
+                                .addComponent(rb_M1)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(rb_F1))
+                            .addComponent(jLabel11)))
+                    .addGroup(jd_RegistroLayout.createSequentialGroup()
+                        .addGap(152, 152, 152)
+                        .addComponent(jButton2)))
+                .addContainerGap(32, Short.MAX_VALUE))
+        );
+        jd_RegistroLayout.setVerticalGroup(
+            jd_RegistroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jd_RegistroLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel5)
+                .addGap(17, 17, 17)
+                .addGroup(jd_RegistroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel7)
+                    .addComponent(tf_registroNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jd_RegistroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel6)
+                    .addComponent(tf_registroUser, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jd_RegistroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel8)
+                    .addComponent(tf_registroContra, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jd_RegistroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(tf_registroEdad, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel9))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel10)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jd_RegistroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(rb_M)
+                    .addComponent(rb_F))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel11)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jd_RegistroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(rb_M1)
+                    .addComponent(rb_F1))
+                .addGap(18, 18, 18)
+                .addComponent(jButton2)
+                .addContainerGap(55, Short.MAX_VALUE))
+        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -85,6 +222,10 @@ public class Tander extends javax.swing.JFrame {
         
     }//GEN-LAST:event_jLabel4MouseClicked
 
+    private void jButton2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton2MouseClicked
+        
+    }//GEN-LAST:event_jButton2MouseClicked
+
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -118,12 +259,31 @@ public class Tander extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.ButtonGroup buttonGroup1;
+    private javax.swing.ButtonGroup buttonGroup2;
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
+    private javax.swing.JDialog jd_Registro;
     private javax.swing.JPasswordField pf_contra;
+    private javax.swing.JRadioButton rb_F;
+    private javax.swing.JRadioButton rb_F1;
+    private javax.swing.JRadioButton rb_M;
+    private javax.swing.JRadioButton rb_M1;
+    private javax.swing.JTextField tf_registroContra;
+    private javax.swing.JTextField tf_registroEdad;
+    private javax.swing.JTextField tf_registroNombre;
+    private javax.swing.JTextField tf_registroUser;
     private javax.swing.JTextField tf_user;
     // End of variables declaration//GEN-END:variables
 }
